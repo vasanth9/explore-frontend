b95bf6aaf0b192c45f2fe731888fe128e3aee602
Vasanth Kumar Cheepurupalli <42891954+vasanth9@users.noreply.github.com>
2025-01-21 09:04:03 +0000

    todo crud app ✅

--- ReactNative/demo-app/app/_layout.tsx
+++ ReactNative/demo-app/app/_layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false , title:'Home'}} />
         <Stack.Screen name="contact" options={{ headerShown: true , title:'Contact', headerTitle:'Contact Us'}} />
         <Stack.Screen name="menu" options={{ headerShown: true , title:'Menu', headerTitle:'Coffee Shop Menu'}} />
+        <Stack.Screen name="todos" options={{ headerShown: false , title:'Todos', headerTitle:'TO DO'}} />
         <Stack.Screen name="+not-found" />
       </Stack>
      
---
+++ ReactNative/demo-app/app/todos.jsx
@@ -0,0 +1,133 @@
+import { View, Text,TextInput , Pressable, StyleSheet, FlatList, } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, {useState} from 'react';
+import {data} from '@/data/todos';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+const todoPage = () => {
+    const [todo,setTodo] = useState(data.sort((a,b)=>b.id-a.id));
+    const [text, setText] = useState('')
+
+    const addTodo = () => {
+        if(text.trim()){
+            const newId = todo.length>0 ? todo[0].id +1 :1;
+            const newTodo = {
+                id: newId,
+                "title": text.trim(),
+                "completed": false
+            }
+            setTodo([newTodo,...todo]);
+            setText('')
+        }
+    }
+
+    const toggleTodo = (id) => {
+        const newTodos = todo.map((t)=>
+          t.id === id ? {...t,
+        completed : !t.completed}: t
+        );
+        setTodo(newTodos);
+    }
+
+    const removeTodo = (id) => {
+        const newTodos = todo.filter((t)=> t.id !=id);
+        setTodo(newTodos)
+    }
+
+    const renderItem = ({item}) => (
+        <View style={styles.todoItem}>
+
+            <Text  style={[styles.todoText, item.completed && styles.completedText]}
+            onPress={()=> toggleTodo(item.id)}
+            >{item.title}</Text>
+            <Pressable onPress={() => removeTodo(item.id)}>
+        <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable={undefined} />
+      </Pressable>
+        </View>
+    )
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+    <View style={styles.inputContainer}>
+    <TextInput
+      style={styles.input}
+      placeholder="Add a new todo"
+      placeholderTextColor="gray"
+      value={text}
+      onChangeText={setText}
+    />
+    <Pressable onPress={addTodo} style={styles.addButton}>
+      <Text style={styles.addButtonText}>Add</Text>
+    </Pressable>
+  </View>
+  <FlatList
+    data={todo}
+    renderItem={renderItem}
+    keyExtractor={todo => todo.id}
+    contentContainerStyle={{ flexGrow: 1 }}
+  />
+
+</SafeAreaView>
+  )
+}
+
+export default todoPage;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'black',
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10,
+      padding: 10,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+    },
+    input: {
+      flex: 1,
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      minWidth: 0,
+      color: 'white',
+    },
+    addButton: {
+      backgroundColor: 'white',
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color: 'black',
+    },
+    todoItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 4,
+      padding: 10,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 1,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      color: 'white',
+    },
+    completedText: {
+      textDecorationLine: 'line-through',
+      color: 'gray',
+    }
+  })
